1ff75597f56e35669457621f9307083a69be25f0
Daku <admin@codeanimu.net>
2016-11-09 17:03:57 +0000

    more cleanup

--- build/files/main.js
+++ build/files/main.js
@@ -1,209 +1,65 @@
-$(function() {
+jQuery(document).ready(function($) {
+
+
+    /*======= Skillset *=======*/
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
+    
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+    
+    
+    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
+    
+    // $("#rss-feeds").rss(
+    //
+    //     //Change this to your own rss feeds
+    //     "http://feeds.feedburner.com/TechCrunch/startups",
+    //
+    //     {
+    //     // how many entries do you want?
+    //     // default: 4
+    //     // valid values: any integer
+    //     limit: 3,
+    //
+    //     // the effect, which is used to let the entries appear
+    //     // default: 'show'
+    //     // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+    //     effect: 'slideFastSynced',
+    //
+    //     // outer template for the html transformation
+    //     // default: "<ul>{entries}</ul>"
+    //     // valid values: any string
+    //     layoutTemplate: "<div class='item'>{entries}</div>",
+    //
+    //     // inner template for each entry
+    //     // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+    //     // valid values: any string
+    //     entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+    //
+    //     }
+    // );
+    
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    GitHubCalendar("#github-graph", "IonicaBizau");
+    
+    
+    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+
 
 });
-
-/*** NOT SORTED ***/
-var lnStickyNavigation;
-
-$(document).ready(function()
-{	
-	applyHeader();
-	applyNavigation(); 
-	applyMailTo();
-	applyResize();
-	checkHash();
-	checkBrowser();
-});
-
-/* HEADER FUNCTIONS */
-
-function applyHeader()
-{
-	$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	
-	lazyLoad($('.jumbotron'));
-}	
-
-function lazyLoad(poContainer)
-{
-	/*var lstrSource   = poContainer.attr('data-src');
-	var lstrPosition = poContainer.attr('data-position');
-
-	$('<img>').attr('src', lstrSource).load(function()
-	{
-		poContainer.css('background-image', 'url("'+ lstrSource +'")');
-		poContainer.css('background-position', lstrPosition);
-		poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
-		poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
-	});*/
-}
-
-/* NAVIGATION FUNCTIONS */
-
-function applyNavigation()
-{
-	applyClickEvent();
-	applyNavigationFixForPhone();
-	applyScrollSpy();
-	applyStickyNavigation();
-}
-
-function applyClickEvent()
-{
-	$('a[href*=#]').on('click', function(e)
-	{
-		e.preventDefault();
-		
-		if( $( $.attr(this, 'href') ).length > 0 )
-		{
-			$('html, body').animate(
-			{
-				scrollTop: $( $.attr(this, 'href') ).offset().top
-			}, 400);
-		}
-		return false;
-	});
-}
-
-function applyNavigationFixForPhone()
-{
-	$('.navbar li a').click(function(event) 
-	{
-		$('.navbar-collapse').removeClass('in').addClass('collapse');
-	});
-}
-
-function applyScrollSpy()
-{
-	$('#navbar-example').on('activate.bs.scrollspy', function() 
-	{
-		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
-	});
-}
-
-function applyStickyNavigation()
-{
-	lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
-	$(window).on('scroll', function() 
-	{  
-		stickyNavigation();  
-	});  
-	
-	stickyNavigation();
-}
-
-function stickyNavigation()
-{         
-	if($(window).scrollTop() > lnStickyNavigation) 
-	{   
-		$('body').addClass('fixed');  
-	} 
-	else 
-	{  
-		$('body').removeClass('fixed');   
-	}  
-}
-
-/* MAILTO FUNCTION */
-
-function applyMailTo()
-{
-	$('a[href*=mailto]').on('click', function(e)
-	{
-		var lstrEmail = $(this).attr('href').replace('mailto:', '');
-		
-		lstrEmail = lstrEmail.split('').reverse().join('')
-		
-		$(this).attr('href', 'mailto:' + lstrEmail);
-	});
-}
-
-/* RESIZE FUNCTION */
-
-function applyResize()
-{
-	$(window).on('resize', function() 
-	{  
-		lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
-		$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	}); 
-}
-
-/* HASH FUNCTION */
-
-function checkHash()
-{
-	lstrHash = window.location.hash.replace('#/', '#');
-	
-	if($('a[href='+ lstrHash +']').length > 0)
-	{
-		$('a[href='+ lstrHash +']').trigger('click');
-	}
-}
-
-/* IE7- FALLBACK FUNCTIONS */
-
-function checkBrowser()
-{
-	var loBrowserVersion = getBrowserAndVersion();
-	
-	if(loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8)
-	{ 
-		$('#upgrade-dialog').modal({
-			backdrop: 'static',
-			keyboard: false
-		});
-	}
-}
-
-function getBrowserAndVersion() 
-{
-	var laBrowserData = [{
-		string: 		navigator.userAgent,
-		subString: 		'MSIE',
-		identity: 		'Explorer',
-		versionSearch: 	'MSIE'
-	}];
-	
-	return {
-		browser: searchString(laBrowserData) || 'Modern Browser',
-		version: searchVersion(navigator.userAgent) || searchVersion(navigator.appVersion) || '0.0'
-	};
-}
-
-function searchString(paData) 
-{
-	for(var i = 0; i < paData.length; i++)	
-	{
-		var lstrDataString 	= paData[i].string;
-		var lstrDataProp 	= paData[i].prop;
-		
-		this.versionSearchString = paData[i].versionSearch || paData[i].identity;
-		
-		if(lstrDataString) 
-		{
-			if(lstrDataString.indexOf(paData[i].subString) != -1)
-			{
-				return paData[i].identity;
-			}
-		}
-		else if(lstrDataProp)
-		{
-			return paData[i].identity;
-		}
-	}
-}
-	
-function searchVersion(pstrDataString) 
-{
-	var lnIndex = pstrDataString.indexOf(this.versionSearchString);
-	
-	if(lnIndex == -1) 
-	{
-		return;
-	}
-	
-	return parseFloat(pstrDataString.substring(lnIndex + this.versionSearchString.length + 1));
-}	
